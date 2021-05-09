1624d7ae503ee3c98277ee33b17000558547d735
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2021-05-09 15:39:07 +0100

    see changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,15 @@ All notable changes to the "vscode-css2stylus" extension will be documented in t
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
+## [1.2.1] 2021-05-09
+### Added
+- Multi-Cursors will get run through the convertor seperately.
+- Bumps url-parse from 1.4.7 to 1.5.1 because security vulnerability.
+
+## [1.1.1] - 2020-05-04
+### Fixed
+- Bumped package.json
+
 ## [1.1.0] - 2020-03-12
 ### Added
 - Command `Convert Stylus to CSS`
--- extension.js
+++ extension.js
@@ -1,48 +1,60 @@
 const vscode = require('vscode')
 const converters = require('./converters.js')
-async function runTransform({codeContent, converter, config}) {
-	if (!codeContent || !codeContent.length) return resolve(null)
-	return converter.run({codeContent, config})
+const awaity = require('awaity')
+async function runTransform ({ codeContent, converter, config }) {
+  if (!codeContent || !codeContent.length) return resolve(null)
+  return converter.run({ codeContent, config })
 }
 
-function transformSelections({editor, key}) {
-	let selections = editor.selections
-	const vscodeConfig = vscode.workspace.getConfiguration('css2stylus', vscode.window.activeTextEditor.document.uri)
-	const converter = converters[key]
-	const config = converter.getConfig({vscodeConfig, editor})
-	return selections.map((selection) => {
-		const codeContent = editor.document.getText(selection)
-		return runTransform({codeContent, converter, config})
-	})
+async function transformSelections ({ editor, converter, selections, config }) {
+  const fragments = await Promise.all(
+    selections.map((selection) => {
+      const codeContent = editor.document.getText(selection)
+      return runTransform({ codeContent, converter, config })
+    })
+  )
+  editor.edit((builder) => {
+    fragments.forEach((fragment, i) => {
+      if (!fragment || !fragment.length) return
+      builder.replace(editor.selections[i], fragment)
+    })
+  })
 }
 
-function replaceSelections(editor, fragments) {
-	editor.edit((builder) => {
-		fragments.forEach((fragment, i) => {
-			if (!fragment || !fragment.length) return
-			builder.replace(editor.selections[i], fragment)
-		})
-	})
+async function transformDocument ({ editor, converter }) {
+  const codeContent = await runTransform({ codeContent: editor.document.getText(), converter, config })
+  let invalidRange = new vscode.Range(0, 0, editor.document.lineCount /*intentionally missing the '-1' */, 0)
+  let fullRange = editor.document.validateRange(invalidRange)
+  editor.edit((edit) => edit.replace(fullRange, sText))
 }
 
-const main = (key) => {
-	let editor = vscode.window.activeTextEditor
-	if (!editor || !converters[key]) return
+const main = async (keyConvertor) => {
+  let editor = vscode.window.activeTextEditor
+  if (!editor || !converters[keyConvertor]) return
 
-	Promise.all(transformSelections({editor, key}))
-		.then((fragments) => replaceSelections(editor, fragments))
-		.catch((err) => {
-			console.error(err)
-		})
+  const vscodeConfig = vscode.workspace.getConfiguration('css2stylus', vscode.window.activeTextEditor.document.uri)
+  const converter = converters[keyConvertor]
+  const config = converter.getConfig({ vscodeConfig, editor })
+
+  try {
+    let selections = editor.selections
+    const bNoSelections = editor.selections.length === 1 && editor.selections[0].isEmpty
+    if (bNoSelections) {
+    }
+    const fragments = await transformSelections({ editor, converter, selections, config })
+    replaceSelections(editor, fragments)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-function activate(context) {
-	Object.keys(converters).map((key) => {
-		let command = vscode.commands.registerCommand(`extension.${key}`, main.bind(null, key))
-		context.subscriptions.push(command)
-	})
+function activate (context) {
+  Object.keys(converters).map((key) => {
+    let command = vscode.commands.registerCommand(`extension.${key}`, main.bind(null, key))
+    context.subscriptions.push(command)
+  })
 }
 exports.activate = activate
 
-function deactivate() {}
+function deactivate () {}
 exports.deactivate = deactivate
